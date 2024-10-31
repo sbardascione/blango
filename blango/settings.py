b69408d0d117c7ff3a51a064eb882c93c4e87757
Carmelo Salamone <crmsal81@gmail.com>
2024-10-31 16:38:16 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -19,6 +19,12 @@ class Dev(Configuration):
   # Build paths inside the project like this: BASE_DIR / 'subdir'.
   BASE_DIR = Path(__file__).resolve().parent.parent
 
+  EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+  ACCOUNT_ACTIVATION_DAYS = 7
+
+  REGISTRATION_OPEN = False
+
   #Used to define List of people who will receive an email logging
   ADMINS = [("Ben Shaw", "ben@example.com"), ("Leo Lucio", "leo@example.com")]
 
