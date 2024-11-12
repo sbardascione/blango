4b996de9165d2108242ecd54692490755c2adda4
Carmelo Salamone <crmsal81@gmail.com>
2024-11-12 09:55:47 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -68,7 +68,8 @@ class Dev(Configuration):
       'allauth',
       'allauth.account',
       'allauth.socialaccount',
-      'allauth.socialaccount.providers.google'
+      'allauth.socialaccount.providers.google',
+      'rest_framework'
   ]
 
   SITE_ID = 1
